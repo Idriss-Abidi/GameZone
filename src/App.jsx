eb53cf3792e2df12da58fcbc2b231ae5c6049f6b
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2024-09-12 00:35:33 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+// import React from 'react'
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Technologies from "./components/Technologies"
+import Experience from "./components/Experience"
+import Project from "./components/Project"
+import Contact from "./components/Contact"
+// import Codenames from "./components/Codenames"
 import Codenames from "./components/Codenames"
 import Cryptage from "./components/Cryptage"
 import Pwd from "./components/Pwd"
@@ -5,16 +14,23 @@ import Mirror from "./components/Mirror"
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-    <div className="fixed top-0 -z-10 h-full w-full">
-    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-    </div> 
+      <div className="fixed top-0 -z-10 h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      </div> 
 
-   <div id="FirstDiv" className="container mx-auto px-20 overflow-auto mb-20">
-   <Cryptage/>
-   <Mirror/>
-   <Codenames />
-   <Pwd/>
-    </div>
+     <div id="FirstDiv" className="container mx-auto px-20  mb-2 flex flex-col items-center py-30">
+     <Cryptage/>
+     <Mirror/>
+     <Codenames />
+     <Pwd/>
+     {/* <Navbar />
+     <Hero/>
+     <About />
+     <Technologies/>
+     <Experience/>
+     <Project/>
+     <Contact/> */}
+     </div>
 
     </div>
   )
