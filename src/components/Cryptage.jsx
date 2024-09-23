d8d625d9d6ae8e139794c07039fc34e63c730f27
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2024-09-23 02:08:12 +0100

    Update Cryptage.jsx

--- src/components/Cryptage.jsx
+++ src/components/Cryptage.jsx
@@ -86,9 +86,9 @@ const Cryptage = ({ addPoints, onSubmit }) => {
                   maxLength="1"
                   className="border-2 border-gray-400 text-gray-800 text-center text-1xl font-bold w-8 h-8"
                   value={inputs[wordIndex][index]}
-{/*                   onChange={(e) => handleInputChange(wordIndex, index, e.target.value)}
-                />
-              ))} */}
+//                  onChange={(e) => handleInputChange(wordIndex, index, e.target.value)}
+  //              />
+    //          ))}
                    onChange={(e) => {
                     handleInputChange(wordIndex, index, e.target.value);
                     if (e.target.value.length === 1 && index < word.length - 1) {
